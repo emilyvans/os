ece624c5a00695c2341e26f4eeb01414ce63872a
Emily van Sleeuwen <troyvansleeuwen@gmail.com>
2025-11-13 00:53:18 +0100

    make cmake more correct and added c header

---
+++ include/stdarg.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_INCLUDE_STDARG_H_
+#define INCLUDE_INCLUDE_STDARG_H_
+
+typedef __builtin_va_list va_list;
+#define va_start(v, l) __builtin_va_start(v, l)
+#define va_end(v) __builtin_va_end(v)
+#define va_arg(v, T) __builtin_va_arg(v, T)
+#define va_copy(d, s) __builtin_va_copy(d, s)
+
+#endif // INCLUDE_INCLUDE_STDARG_H_
---
+++ include/stddef.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_INCLUDE_STDDEF_H_
+#define INCLUDE_INCLUDE_STDDEF_H_
+
+typedef unsigned long size_t;
+
+#define NULL 0
+
+#endif // INCLUDE_INCLUDE_STDDEF_H_
