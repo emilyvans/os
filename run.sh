f8600a48e7134f3bb7a5d96cd48a2af9e067795f
Emily van Sleeuwen <troyvansleeuwen@gmail.com>
2026-03-10 00:03:00 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,12 @@ qemu-system-x86_64 \
 	-m 4096 \
 	-monitor telnet:127.0.0.1:7777,server,nowait \
 	-serial mon:stdio \
-	-drive format=raw,file=drive.hdd \
+	-drive format=raw,file=drive.hdd,if=none,id=disk0 \
+	-device virtio-blk-pci,drive=disk0 \
 	-device virtio-sound \
 	-device VGA \
 	-device virtio-gpu-pci \
+	-boot order=c \
 	-drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_CODE.4m.fd \
+	-drive if=pflash,format=raw,file=OVMF_VARS.4m.fd \
 	$@
