53a2e98eceb4dcdc8f80f26cd9fc2c1eb5931382
Emily van Sleeuwen <troyvansleeuwen@gmail.com>
2026-03-09 23:36:16 +0100

    added more I/O port functions

--- include/cpu/asm.hpp
+++ include/cpu/asm.hpp
@@ -3,7 +3,11 @@
 #include <stdint.h>
 
 void outb(uint16_t address, uint8_t data);
+void outw(uint16_t address, uint16_t data);
+void outl(uint16_t address, uint32_t data);
 uint8_t inb(uint16_t address);
+uint16_t inw(uint16_t address);
+uint32_t inl(uint16_t address);
 void io_wait();
 
 #endif // INCLUDE_CPU_ASM_HPP_
--- src/cpu/asm.cpp
+++ src/cpu/asm.cpp
@@ -1,12 +1,32 @@
 #include "cpu/asm.hpp"
 
 void outb(uint16_t address, uint8_t data) {
-	asm volatile("outb %b0, %w1" : : "a"(data), "Nd"(address) : "memory");
+	asm volatile("outb %0, %w1" : : "a"(data), "Nd"(address) : "memory");
+}
+
+void outw(uint16_t address, uint16_t data) {
+	asm volatile("outw %0, %w1" : : "a"(data), "Nd"(address) : "memory");
+}
+
+void outl(uint16_t address, uint32_t data) {
+	asm volatile("outl %0, %w1" : : "a"(data), "Nd"(address) : "memory");
 }
 
 uint8_t inb(uint16_t address) {
 	uint8_t data;
-	asm volatile("inb %1, %0" : "=a"(data) : "d"(address));
+	asm volatile("inb %1, %0" : "=a"(data) : "Nd"(address));
+	return data;
+}
+
+uint16_t inw(uint16_t address) {
+	uint16_t data;
+	asm volatile("inw %1, %0" : "=a"(data) : "Nd"(address));
+	return data;
+}
+
+uint32_t inl(uint16_t address) {
+	uint32_t data;
+	asm volatile("inl %1, %0" : "=a"(data) : "Nd"(address));
 	return data;
 }
 
